885f1e72cb35d9716dfe0eae51e0e209fe296da8
yulia <20051992yuyu@gmail.com>
2022-11-29 14:02:46 +0200

    test using

--- lib/test_plug_auth.ex
+++ lib/test_plug_auth.ex
@@ -23,6 +23,26 @@ defmodule TestPlugAuth do
       plug Guardian.Plug.VerifyHeader, realm: options[:realm]
       plug Guardian.Plug.EnsureAuthenticated
       plug Guardian.Plug.LoadResource
+      use Guardian, otp_app: options[:otp_app]
+
+      def subject_for_token(user, _claims) do
+        sub = to_string(user.id)
+        {:ok, sub}
+      end
+
+      def subject_for_token(_, _) do
+        {:error, :reason_for_error}
+      end
+
+      def resource_from_claims(claims) do
+        id = claims["sub"]
+        resource = MyApi.Accounts.get_user!(id)
+        {:ok,  resource}
+      end
+
+      def resource_from_claims(_claims) do
+        {:error, :reason_for_error}
+      end
     end
     IO.puts("USING")
   end
