7138ba19a4cbad0a240228dccfe35d682744a103
yulia <20051992yuyu@gmail.com>
2022-11-30 10:57:40 +0200

    new vertion

--- lib/test_plug_auth.ex
+++ lib/test_plug_auth.ex
@@ -4,8 +4,11 @@ defmodule TestPlugAuth do
     IO.puts("options: #{inspect options}")
     options
   end
-  def call(conn, _options) do
-    IO.puts("options: #{inspect conn}")
+  def call(conn, options) do
+    IO.puts("conn: #{inspect conn}")
+    IO.puts("user_id: #{inspect conn.body_params[options.key]}")
+    IO.puts("token: #{inspect conn.req_headers["authorization"]}")
+    IO.puts("options_call: #{inspect options}")
     conn
   end
 end
