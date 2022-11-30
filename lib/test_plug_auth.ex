a9af5e273152c6fc66d507db2ae631d37d1a8597
yulia <20051992yuyu@gmail.com>
2022-11-30 11:31:26 +0200

    new ver

--- lib/test_plug_auth.ex
+++ lib/test_plug_auth.ex
@@ -8,7 +8,7 @@ defmodule TestPlugAuth do
     IO.puts("conn: #{inspect conn}")
     IO.puts("user_id: #{inspect conn.body_params[options[:key]]}")
     IO.puts("req_headers: #{inspect conn.req_headers}")
-    auth = Enum.each(conn.req_headers, fn {k,v} -> if k == "authorization" do v end end)
+    auth = Enum.each(conn.req_headers, fn {k,v} -> if k == "authorization" do IO.puts(v) end end)
     IO.puts("authorization: #{inspect auth}")
     IO.puts("token: #{inspect conn.req_headers[:plug_session_fetch][:authorization]}")
     IO.puts("options_call: #{inspect options}")
