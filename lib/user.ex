4c019c9c62ad34a142c81c0f1911b94b2a534400
yulia <20051992yuyu@gmail.com>
2022-12-06 11:13:32 +0200

    gen auth

--- lib/user.ex
+++ lib/user.ex
@@ -1,6 +1,6 @@
 defmodule TestPlugAuth.User do
   def validate_user_id(user_id, sub, resource) do
-    if user_id == String.to_integer(sub) && String.to_integer(sub) == resource.id do
+    if user_id == sub && sub == resource.id do
       {:ok, :authorized}
     else
       {:error, :unauthorized}
